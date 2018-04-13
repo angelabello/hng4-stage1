e9442e770288e2e0a6d400b69de67783d3cda0bf
Angela Bello <angelabisibello@gmail.com>
2018-04-13 11:01:21 +0200

    add stage 1 files to repo

---
+++ page.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Angela Bello - Stage 1</title>
+</head>
+<body>
+    <div class="content">
+        <article>
+            <h4>Quote of the day</h4>
+            <div class="quote">
+                <q class="quote-text">Life is but thought.</q>
+                <h4 id="author">Sara Teasdale</h4>
+            </div>
+        </article>
+        <div class="clock">
+            <h3 id="date"><?php echo date("d F Y"); ?></h3>
+            <h1 id="time"><?php echo date("H:i:s"); ?></h1>
+        </div>
+    </div>
+    <div id="footer">
+        <footer>Designed by Angela Bello for HNG Internship 4.0</footer>
+    </div>
+</body>
+</html>
